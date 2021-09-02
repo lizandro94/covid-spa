98437525e522233ac32014691887d140e00251e5
Leandro Fonseca <leandrofonsecaalfaro@hotmail.com>
2021-09-02 16:57:12 -0600

    Add react router and ant design packages

---
+++ src/Router.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Loader from './components/Loader/Loader';
+import loadable from "@loadable/component";
+
+const routes = [
+    {
+        path: "/",
+        component: loadable(() => import("./components/countries/Countries"), { fallback: <Loader /> }),
+        exact: true
+    }
+];
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                {routes.map(route => <Route path={route.path} component={() => <route.component />} key={route.path} exact={route.exact} />)}
+            </Switch>
+        </BrowserRouter>
+    );
+};
+
+export default Router;
